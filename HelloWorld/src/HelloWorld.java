a54a54e1a67d975b46cd382f0ee6d5fc08c1270f
vmann <vmann@DESKTOP-JD1DIRN>
2021-01-28 12:30:27 -0500

    This is my "Hello World" project.

---
+++ HelloWorld/src/HelloWorld.java
@@ -0,0 +1,15 @@
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World");
+		System.out.println(5);
+		System.out.print("2021");
+		System.out.print("January");
+		System.out.println("");
+		System.out.println(1+5);
+		System.out.println("1+5");
+	}
+
+}
